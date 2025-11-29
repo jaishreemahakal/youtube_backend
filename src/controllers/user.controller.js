33331e02207ad34301d89d101d84f721929629d5
Abhishek <abhishek23jangir@gmail.com>
2025-11-29 15:45:54 +0530

    add register controller and bug fixes

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,10 +1,44 @@
 import {asyncHandler} from '../utils/asyncHandlers.js';
+import {ApiError} from '../utils/apiError.js';
+import { User } from '../models/user.model.js';
 
 
 const registerUser = asyncHandler(async (req, res) => {
-  // Logic for registering a user
-  res.status(200).json(
-    { message: "ok" });
+  //gets users details from frontend
+  //vaildation 
+  //cheks id users already exists :username,email
+  //checks of images and avatars
+  //upload it to the clodinary
+  //create user objec ,create entry in db
+  //remove password and refreshtoken from the response
+  //checks for user creation is yes
+  //return response to frontend
+  //else throw error 
+
+  //destructuring user details from req.body
+  const { fullName,email,username,password}=req.body;
+  console.log("email:",email);
+
+  //vaildation
+  if([fullName,email,username,password].some((field)=>field?.trim()===""))
+  {
+    throw new ApiError("All fields are required",400);
+  }
+  //check if user already exists
+
+  const existedUser= await User.findOne({
+    $or:[{email},{username}]
+  })
+  if(existedUser){
+    throw new ApiError("User already exists with this email or username",409);
+  }
+
+  //check for images and avatars
+
+  const avtarlocalpath=req.files?.avatar[0]?.path;
+  const coverImagelocalpath=req.files?.coverImage[0]?.path;
+  
+  
 })
 
 export {registerUser,}
--- src/routes/user.router.js
+++ src/routes/user.router.js
@@ -1,10 +1,21 @@
 import { Router } from "express";
 import { registerUser } from "../controllers/user.controller.js";
 // what is router in express?
+import {upload} from "../middlewares/multer.middleware.js";
 
 const router=Router();
 
-router.route("/register").post(registerUser);
+router.route("/register").post(upload.fields(
+    [
+     {  name:"avtar",
+        maxCount:1
+     },
+     {  name:"coverImage",
+        maxCount:1
+     }   
+
+    ]
+),registerUser);
 
 
 
